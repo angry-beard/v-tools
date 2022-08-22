7d8ff169c53a089af794301699b7cd7c6a9bdd1c
angry_beard <better_river@163.com>
2022-08-22 11:28:16 +0800

    add doc

---
+++ README.md
@@ -0,0 +1,3 @@
+# v-tools
+
+[咕泡](https://ke.gupaoedu.cn/home) 笔记
---
+++ v-elasticsearch/src/test/java/GenData.java
@@ -0,0 +1,11 @@
+/**
+ * @author angry_beard
+ * @date 2022/8/11 17:51
+ */
+public class GenData {
+
+    public static void main(String[] args) {
+        // 字符过滤器
+        System.out.println(800000 * 0.014 * 36);
+    }
+}
